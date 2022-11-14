f6fefd34c2a2b8721ded846ae59a41bc52381ad7
Crashkonya <3d-war@mail.ru>
2022-11-14 19:40:13 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	stan "github.com/nats-io/stan.go"
 	log "log"
 	"os"
-	"sync"
 	"time"
 	//"github.com/shijuvar/gokit/examples/nats-streaming/pb"
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -16,7 +15,6 @@ import (
 var sqlmsg ordWB
 var err error
 var retID int8
-var wg sync.WaitGroup
 
 const (
 	clusterID = "wb_cluster"
@@ -37,49 +35,44 @@ func main() {
 	}
 	// Close connection
 	defer sc.Close()
-	wg.Add(1)
-	go func() {
-		sub, err := sc.Subscribe(channel, func(m *stan.Msg) {
 
-			err = json.Unmarshal(m.Data, &sqlmsg)
-			if err != nil {
-				log.Println(err)
-			}
-			fmt.Println(sqlmsg.Delivery.Name)
-			db, err := sql.Open("pgx", "postgres://postgres:Parol123!@localhost:5432/wb_l0")
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-				os.Exit(1)
-			}
-			err = db.QueryRow("INSERT INTO orders(id,order_uid,track_number,entry,locale,internal_signature,customer_id,delivery_service,shardkey,sm_id,date_created,oof_shard) VALUES (default, $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING id", sqlmsg.OrderUID, sqlmsg.TrackNumber, sqlmsg.Entry, sqlmsg.Locale, sqlmsg.InternalSignature, sqlmsg.CustomerID, sqlmsg.DeliveryService, sqlmsg.Shardkey, sqlmsg.SmID, sqlmsg.DateCreated, sqlmsg.OofShard).Scan(&retID)
-			if err != nil {
-				log.Println(err)
-			}
-			_, err = db.Exec("INSERT INTO delivery(id,name,phone,zip,city,adress,region,email,orderid) VALUES (default,$1, $2, $3, $4, $5, $6, $7, $8)", sqlmsg.Delivery.Name, sqlmsg.Delivery.Phone, sqlmsg.Delivery.Zip, sqlmsg.Delivery.City, sqlmsg.Delivery.Address, sqlmsg.Delivery.Region, sqlmsg.Delivery.Email, retID)
-			if err != nil {
-				log.Println(err)
-			}
-			_, err = db.Exec("INSERT INTO payment(id,transaction,request_id,currency,provider,amount,payment_dt,bank,delivery_cost,goods_total,custom_fee,orderid) VALUES (default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)", sqlmsg.Payment.Transaction, sqlmsg.Payment.RequestID, sqlmsg.Payment.Currency, sqlmsg.Payment.Provider, sqlmsg.Payment.Amount, sqlmsg.Payment.PaymentDt, sqlmsg.Payment.Bank, sqlmsg.Payment.DeliveryCost, sqlmsg.Payment.GoodsTotal, sqlmsg.Payment.CustomFee, retID)
-			if err != nil {
-				log.Println(err)
-			}
-			_, err = db.Exec("INSERT INTO items(id,chrt_id,track_number,price,rid,name,sale,size,total_price,nm_id,brand,status,orderid) VALUES (default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)", sqlmsg.Items[0].ChrtID, sqlmsg.Items[0].TrackNumber, sqlmsg.Items[0].Price, sqlmsg.Items[0].Rid, sqlmsg.Items[0].Name, sqlmsg.Items[0].Sale, sqlmsg.Items[0].Size, sqlmsg.Items[0].TotalPrice, sqlmsg.Items[0].NmID, sqlmsg.Items[0].Brand, sqlmsg.Items[0].Status, retID)
-			if err != nil {
-				fmt.Println(err)
-			}
-			wg.Done()
+	sub, err := sc.Subscribe(channel, func(m *stan.Msg) {
 
-			defer db.Close()
-		}, stan.StartWithLastReceived())
+		err = json.Unmarshal(m.Data, &sqlmsg)
 		if err != nil {
 			log.Println(err)
 		}
-		//res, err := db.Exec //работа с SQL -
-		defer sub.Unsubscribe()
+		fmt.Println(sqlmsg.Delivery.Name)
+		db, err := sql.Open("pgx", "postgres://postgres:Parol123!@localhost:5432/wb_l0")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+			os.Exit(1)
+		}
+		err = db.QueryRow("INSERT INTO orders(id,order_uid,track_number,entry,locale,internal_signature,customer_id,delivery_service,shardkey,sm_id,date_created,oof_shard) VALUES (default, $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING id", sqlmsg.OrderUID, sqlmsg.TrackNumber, sqlmsg.Entry, sqlmsg.Locale, sqlmsg.InternalSignature, sqlmsg.CustomerID, sqlmsg.DeliveryService, sqlmsg.Shardkey, sqlmsg.SmID, sqlmsg.DateCreated, sqlmsg.OofShard).Scan(&retID)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = db.Exec("INSERT INTO delivery(id,name,phone,zip,city,adress,region,email,orderid) VALUES (default,$1, $2, $3, $4, $5, $6, $7, $8)", sqlmsg.Delivery.Name, sqlmsg.Delivery.Phone, sqlmsg.Delivery.Zip, sqlmsg.Delivery.City, sqlmsg.Delivery.Address, sqlmsg.Delivery.Region, sqlmsg.Delivery.Email, retID)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = db.Exec("INSERT INTO payment(id,transaction,request_id,currency,provider,amount,payment_dt,bank,delivery_cost,goods_total,custom_fee,orderid) VALUES (default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)", sqlmsg.Payment.Transaction, sqlmsg.Payment.RequestID, sqlmsg.Payment.Currency, sqlmsg.Payment.Provider, sqlmsg.Payment.Amount, sqlmsg.Payment.PaymentDt, sqlmsg.Payment.Bank, sqlmsg.Payment.DeliveryCost, sqlmsg.Payment.GoodsTotal, sqlmsg.Payment.CustomFee, retID)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = db.Exec("INSERT INTO items(id,chrt_id,track_number,price,rid,name,sale,size,total_price,nm_id,brand,status,orderid) VALUES (default,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)", sqlmsg.Items[0].ChrtID, sqlmsg.Items[0].TrackNumber, sqlmsg.Items[0].Price, sqlmsg.Items[0].Rid, sqlmsg.Items[0].Name, sqlmsg.Items[0].Sale, sqlmsg.Items[0].Size, sqlmsg.Items[0].TotalPrice, sqlmsg.Items[0].NmID, sqlmsg.Items[0].Brand, sqlmsg.Items[0].Status, retID)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	}()
+		defer db.Close()
+	}, stan.StartWithLastReceived())
+	if err != nil {
+		log.Println(err)
+	}
+	//res, err := db.Exec //работа с SQL -
+	defer sub.Unsubscribe()
 
-	wg.Wait()
 	fmt.Println("Обработка выполнена!")
 }
 
